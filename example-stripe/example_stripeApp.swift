af92a6f2a7e785de454a7dcccafb9ead2cefc610
thomas.mello <mellot99@gmail.com>
2021-10-19 16:40:25 -0500

    Initial Commit

---
+++ example-stripe/example_stripeApp.swift
@@ -0,0 +1,17 @@
+//
+//  example_stripeApp.swift
+//  example-stripe
+//
+//  Created by Thomas Mello on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct example_stripeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
